e2a127e1e8b7dbc0e2803909e5288348e394d187
Ajyothsna758 <jyoece758@gmail.com>
2025-12-12 20:43:14 +0000

    setup view templates

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    first_name= models.CharField(max_length=100)
+    middle_name=models.CharField(max_length=100)
+    surname=models.CharField(max_length=100)
+    email=models.EmailField()
+    contact_number=models.IntegerField()
+    notice_period= models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.first_name}+{self.middle_name}+{self.surname}"
+    
+    
 class Skill(models.Model):
     name= models.CharField(max_length=225)
     level= models.CharField(max_length=100)
